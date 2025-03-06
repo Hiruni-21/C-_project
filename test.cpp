8a6d83d0534d61f94d6f22568cbf0b04d7182f3e
Hruni-21 <wedagehiruni123@gmail.com>
2025-03-06 16:49:45 +0530

    Add SimpleAddition C++ program and README

---
+++ test.cpp
@@ -0,0 +1,16 @@
+// SimpleAddition.cpp
+
+#include <iostream>
+using namespace std;
+
+int addNumbers(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << addNumbers(num1, num2) << endl;
+    return 0;
+}
+
